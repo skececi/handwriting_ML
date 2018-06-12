6c264100e3443d17f99a4839cb656494611d4412
Samuel Kececi <skececi@internal.salesforce.com>
2018-06-12 15:55:51 -0400

    updated model, image converter

--- handwriting.py
+++ handwriting.py
@@ -20,8 +20,11 @@ plt.imshow(X_train[5000], cmap=plt.get_cmap('gray'))
 plt.show()
 
 num_pixels = X_train.shape[1] * X_train.shape[2]
-X_train = X_train.reshape(1, num_pixels).astype('float32') / 255
-X_test = X_test.reshape(1, num_pixels).astype('float32') / 255
+# num_pixels = 28 * 28 = 784
+X_train = X_train.reshape(X_train.shape[0], num_pixels).astype('float32')
+X_train = X_train / 255
+X_test = X_test.reshape(X_test.shape[0], num_pixels).astype('float32') / 255
+X_test = X_test / 255
 
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
@@ -35,7 +38,7 @@ model.fit(X_train, y_train, validation_data=(X_test, y_test))
 def create_model():
     model = Sequential()
 
-    model.add(Dense(500, activation='relu', input_shape=(784,)))
+    model.add(Dense(num_pixels, activation='relu', input_shape=(num_pixels,)))
     model.add(Dense(10, activation='softmax'))
 
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
